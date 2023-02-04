9c3937f29a10edc2d7065c1f991b3f0bba1c8fd5
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-03 22:17:59 -0300

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Trello - Nuxt 3',
+    },
+  },
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', 'nuxt-icon'],
 });
