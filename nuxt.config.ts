574bc04547af598794e81199dd2d068efbebaef9
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-10 22:39:42 -0300

    chore: add transition pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,12 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      charset: 'utf-8',
+      viewport: 'initial-scale=1',
       title: 'Trello - Nuxt 3',
+      meta: [{ name: 'description', content: 'Trello with Nuxt 3' }],
     },
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
   typescript: {
     strict: true,
