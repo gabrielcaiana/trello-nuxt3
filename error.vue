8d0ae06c559532df8a59378a9f1c3b8a0cec168f
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-10 22:17:47 -0300

    feat: add page error

---
+++ error.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+const message = computed(() => String(props.error?.message || ''));
+const is404 = computed(
+  () => props.error?.statusCode == 404 || message.value?.includes('404')
+);
+const isDev = process.dev;
+const handleError = () => clearError({ redirect: '/' });
+</script>
+<template>
+  <NuxtLayout name="error">
+    <div class="flex flex-col items-center mt-12 text-white">
+      <div class="text-2xl mb-2">
+        {{ is404 ? 'Página não encontrada' : 'Ocorreu um erro inesperado' }}
+      </div>
+      <div class="text-xl text-neutral-500">
+        Parece que você seguiu um link quebrado ou inseriu um URL que não
+        existem neste site.
+      </div>
+      <pre class="my-4" v-if="isDev">{{ error }}</pre>
+      <button
+        class="bg-neutral-400 text-white p-3 rounded-md hover:bg-neutral-600"
+        @click="handleError"
+      >
+        Retornar ao início
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-zinc-900">
+    <div class="container mx-auto py-10">
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
