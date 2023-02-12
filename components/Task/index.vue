5584cad81e610e6df392cc38ea82f5d94e93258a
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-12 15:26:15 -0300

    fix: size drag icon

--- components/Task/index.vue
+++ components/Task/index.vue
@@ -25,8 +25,12 @@ onKeyStroke('Backspace', (_e) => {
       tabindex="0"
       class="p-3 mt-4 mb-2 bg-zinc-500 rounded shadow-sm max-w-[280px] flex gap-2 focus:shadow focus:border focus:border-zinc-200 cursor-pointer"
     >
-      <DragHandle class="mt-1" />
-      <span datatest-id="task-title" class="text-white">{{ task.title }}</span>
+      <div>
+        <DragHandle class="mt-1" />
+      </div>
+      <span datatest-id="task-title" class="text-white break-all">{{
+        task.title
+      }}</span>
     </div>
   </div>
 </template>
